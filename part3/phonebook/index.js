9bb2d3dee7a407dd606c80702d92c43b07bba178
Ilifilza Rusli <ilifilza@snappymob.com>
2021-08-29 23:26:23 +0800

    Add frontend build folder

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-
+app.use(express.static('build'));
 var morgan = require("morgan");
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(
