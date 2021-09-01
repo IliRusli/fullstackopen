4f53ec30f0108e677d7d50c556c8ec81d9a3b233
Ilifilza Rusli <ilifilza@snappymob.com>
2021-09-01 23:48:22 +0800

    Implement eslint to the project

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
   console.log(
-      'Please provide the password as an argument: node mongo.js <password>');
+    'Please provide the password as an argument: node mongo.js <password>'
+  );
   process.exit(1);
 }
 
@@ -10,10 +12,9 @@ const password = process.argv[2];
 const name = process.argv[3];
 const phoneNumber = process.argv[4];
 
-const url = `mongodb+srv://fullstackopen:${
-    password}@fullstackopen.bwtvi.mongodb.net/fullstackopen?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullstackopen:${password}@fullstackopen.bwtvi.mongodb.net/fullstackopen?retryWrites=true&w=majority`;
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const personSchema = new mongoose.Schema({
   name: String,
@@ -28,7 +29,7 @@ if (name && phoneNumber) {
     number: phoneNumber,
   });
 
-  person.save().then(result => {
+  person.save().then(() => {
     console.log(`Added ${name} number ${phoneNumber} to phonebook`);
     mongoose.connection.close();
   });
@@ -38,5 +39,5 @@ if (name && phoneNumber) {
       console.log(`${person.name} ${person.number}`);
     });
     mongoose.connection.close();
-  })
+  });
 }
