a2d65c4be9d156cc9c26a52ff125988de5bd6feb
Ilifilza Rusli <ilifilza@snappymob.com>
2021-10-04 21:56:42 +0800

    Exercise 4.13 - 4.14

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -19,4 +19,42 @@ blogRouter.post('/', async (request, response, next) => {
     .catch(error => next(error));
 });
 
+blogRouter.get('/:id', async (request, response) => {
+  const id = request.params.id;
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+    // Yes, it's a valid ObjectId, proceed with `findById` call.
+    const blog = await Blog.findById(id);
+
+    if (blog) {
+      response.json(blog.toJSON());
+    } else {
+      response.status(404).end();
+    }
+  } else {
+    response.status(400).end();
+  }
+});
+
+blogRouter.put('/:id', (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON());
+    })
+    .catch(error => next(error));
+});
+
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
 module.exports = blogRouter;
--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -6,115 +6,147 @@ const bloglistHelper = require('../utils/bloglist_helper');
 const api = supertest(app);
 
 const Blog = require('../models/blog');
-const initialBlogs = [
-  {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-  },
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url:
-      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  },
-];
+const helper = require('./test_helper');
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  let blogObject = new Blog(initialBlogs[0]);
+  let blogObject = new Blog(helper.initialBlogs[0]);
   await blogObject.save();
-  blogObject = new Blog(initialBlogs[1]);
+  blogObject = new Blog(helper.initialBlogs[1]);
   await blogObject.save();
 });
 
-test('bloglist are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-}, 100000);
+describe('when there is initially some blogs saved', () => {
+  test('bloglist are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  }, 100000);
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
-  expect(response.body).toHaveLength(initialBlogs.length);
-});
+  test('all blogs are returned', async () => {
+    const response = await helper.blogsInDb();
+    expect(response).toHaveLength(helper.initialBlogs.length);
+  });
 
-test('a specific author is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs');
+  test('a specific author is within the returned blogs', async () => {
+    const response = await helper.blogsInDb();
 
-  expect(response.body[1].author).toBe('Edsger W. Dijkstra');
-});
+    expect(response[1].author).toBe('Edsger W. Dijkstra');
+  });
 
-test('blog unique id is defined', async () => {
-  const response = await api.get('/api/blogs');
-  response.body.forEach(blog => {
-    expect(blog.id).toBeDefined();
+  test('blog unique id is defined', async () => {
+    const response = await api.get('/api/blogs');
+    response.body.forEach(blog => {
+      expect(blog.id).toBeDefined();
+    });
   });
 });
 
-test('a valid blog post can be added', async () => {
-  const newBlog = {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url:
-      'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-  };
+describe('viewing a specific blog', () => {
+  test('succeeds with a valid id', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToView = blogsAtStart[0];
+    const resultBlog = await api
+      .get(`/api/blogs/${blogToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    const processedBlogToView = JSON.parse(JSON.stringify(blogToView));
 
-  const response = await api.get('/api/blogs');
+    expect(resultBlog.body).toEqual(processedBlogToView);
+  });
 
-  const titles = response.body.map(r => r.title);
+  test('fails with statuscode 404 if blog does not exist', async () => {
+    const validNonexistingId = await helper.nonExistingId();
+    console.log({ validNonexistingId });
 
-  expect(response.body).toHaveLength(initialBlogs.length + 1);
-  expect(titles).toContain('First class tests');
+    await api.get(`/api/blogs/${validNonexistingId}`).expect(404);
+  });
+
+  test('fails with statuscode 400 id is invalid', async () => {
+    const invalidId = '5a3d5da59070081a82a3445';
+
+    await api.get(`/api/blogs/${invalidId}`).expect(400);
+  });
 });
 
-test('blog likes property default to 0', async () => {
-  const newBlog = {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url:
-      'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-  };
+describe('addition of a new blog', () => {
+  test('succeeds with valid data', async () => {
+    const newBlog = {
+      title: 'Learn React',
+      author: 'Michael X',
+      url: 'https://reactpatterns.com/',
+      likes: 9,
+    };
 
-  const checkedPayload = await bloglistHelper.preparePayload(newBlog);
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-  await api
-    .post('/api/blogs')
-    .send(checkedPayload)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-  const response = await api.get('/api/blogs');
+    const titles = blogsAtEnd.map(n => n.title);
+    expect(titles).toContain('Learn React');
+  });
 
-  expect(response.body[2].likes).toBe(0);
+  test('fails with status code 400 if data invalid', async () => {
+    const newBlog = {
+      likes: 5,
+      url:
+        'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const response = await helper.blogsInDb();
+
+    expect(response).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('blog likes property default to 0', async () => {
+    const newBlog = {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url:
+        'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    };
+
+    const checkedPayload = await bloglistHelper.preparePayload(newBlog);
+
+    await api
+      .post('/api/blogs')
+      .send(checkedPayload)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const response = await helper.blogsInDb();
+    expect(response[2].likes).toBe(0);
+  });
 });
 
-test('blog without title and author is not added', async () => {
-  const newBlog = {
-    likes: 5,
-    url:
-      'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-  };
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect('Content-Type', /application\/json/);
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-  const response = await api.get('/api/blogs');
+    const blogsAtEnd = await helper.blogsInDb();
 
-  expect(response.body).toHaveLength(initialBlogs.length);
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    const authors = blogsAtEnd.map(r => r.author);
+
+    expect(authors).not.toContain(blogToDelete.author);
+  });
 });
 
 afterAll(() => {
